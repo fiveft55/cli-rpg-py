0c9532a89501dc67375f622e76a99934df3234e6
src alt <52785691+KoldKardio@users.noreply.github.com>
2023-11-06 19:27:36 +1100

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 
 run = True
 menu = True
@@ -12,7 +12,11 @@ pot = 1
 mana = 0
 gold = 0
 x, y = 0, 0
-key = 0
+key = False
+standing = True
+buy = False
+speak = False
+boss = False
 
 #  create maps
         # col0    #col1    #col2    # col3   #col4   #col5    #col x=6
@@ -24,6 +28,33 @@ map = [["plains", "plains", "plains", "plains", "forest", "mountain", "cave"], #
 
 y_len = len(map)-1
 x_len = len(map[0])-1
+
+# enemy list
+e_list = ['Goblin', 'Orc', 'Slime']
+
+mobs = {
+    "Goblin": {
+        'hp': 15,
+        'at': 3,
+        'go': 8
+    },
+    "Orc": {
+        'hp': 35,
+        'at': 5,
+        'go': 18
+    },
+    "Slime": {
+        'hp': 30,
+        'at': 2,
+        'go': 12
+    },
+    # boss
+    "Dragon": {
+        'hp': 100,
+        'at': 8,
+        'go': 100
+    },
+}
 
 # create biom
 biom = {
@@ -79,7 +110,7 @@ name_of_tile = biom[current_tile]["t"]
 enemy_tile = biom[current_tile]["e"]
 # print(enemy_tile)
 
-# player status 
+# player status - organize in future ::ref
 def player_stat():
     pass
 
@@ -89,6 +120,109 @@ def clear():
 
 def draw():
     print("xx--------------------------------xx")
+
+# final boss
+def cave():
+    global boss, key, name, fight
+
+    while boss:
+        clear()
+        draw()
+        print("...HERE LIES THE CAVE OF THE DRAGON...")
+        print("WHAT WILL YOU DO, " + name + "?")
+        draw()
+        if key:
+            print("1 - USE KEY!")
+        print("2 - TURN BACK!")
+        draw()
+
+        choice = input(">: ")
+
+        if choice == '1':
+            if key:
+                fight = True
+                battle()
+
+        elif choice == '2':
+            boss = False
+
+
+def mayor():
+    global speak, key
+
+    while speak:
+        clear()
+        draw()
+        print("Hello there, " + name + "!")
+        if ATK < 10:
+            print("You're not strong enough to face the dragon yet!" + "\nCome back after you become stronger!")
+            key = False
+        else:
+            print("You might defeat the dragon! Take this key, warrior! And find the cave...")
+            key = True
+            pass
+        
+        draw()
+        print("1 - LEAVE")
+        draw()
+
+        choice = input("#: ")
+
+        if choice == '1':
+            speak = False
+
+# shop func
+def shop():
+    global buy, gold, pot, mana, ATK
+
+    while buy:
+        clear()
+        draw()
+        print("WELCOME TO THE SHOP!")
+        draw()
+        print("GOLD: " + str(gold))
+        print("POTIONS: " + str(pot))
+        print("MANA: " + str(mana))
+        print("ATK: " + str(ATK))
+        draw()
+        print("1 - BUY POTION (30HP): 5 GOLD")
+        print("2 - BUY MANA (50HP): 10 GOLD")
+        print("3 - UPGRADE ATK (+2): 25 GOLD")
+        print("4 - LEAVE")
+        draw()
+
+        choice = input(">: ")
+
+        if choice == '1':
+            if gold >= 5:
+                pot += 1
+                gold -= 5
+                print("You bought potion!")
+            else:
+                print("You don't have money!")
+            input(">: ")
+            pass
+        elif choice == '2':
+            if gold >= 10:
+                mana += 1
+                gold -= 10
+                print("You bought mana!")
+            else:
+                print("You don't have money!")
+            input(">: ")
+            pass
+        elif choice == '3':
+            if gold >= 25:
+                ATK += 2
+                gold -= 25
+                print("You bought an upgrade!")
+            else:
+                print("You don't have money!")
+            input(">: ")
+            pass
+        elif choice == '4':
+            buy = False
+
 
 # func for savin files
 def save():
@@ -109,6 +243,110 @@ def save():
         f.write(item + "\n")
     f.close()
 
+# heal func
+def heal(amount):
+    global HP
+    if HP + amount < MAXHP:
+        HP += amount
+    else:
+        HP = MAXHP
+    print(name + "'s HP refilled to " + str(HP) +"!")
+    
+# battle mode
+def battle():
+    global fight, play, run, HP, pot, mana, gold, boss
+    # print("Commence fightin!")
+    # input(">: ")
+    if not boss:
+        enemy =  random.choice(e_list)
+    else:
+        enemy = "Dragon"
+    hp = mobs[enemy]['hp']
+    hpmax = hp
+    atk = mobs[enemy]['at']
+    g = mobs[enemy]['go']
+
+    # fight loop
+    while fight:
+        clear()
+        draw()
+        print("Defeat the " + enemy + "!")
+        draw()
+        print(enemy + "'s HP: " + str(hp) + "/" + str(hpmax))
+        print(name + "'s HP: " + str(HP) + "/" + str(MAXHP))
+        print("POTIONS: " + str(pot))
+        print("MANA: " + str(mana))
+        draw()
+        print("1 - ATTACK")
+        if pot > 0:
+            print("2 - USE POTION (30HP)")
+        if mana > 0:
+            print("3 - USE MANA (50HP)")
+        if pot == 0 and mana == 0: print("NO ITEMS or MAGIC")
+        draw()
+
+        choice = input("# ")
+
+        if choice == "1":
+            hp -= ATK
+            print(name + " dealt " + str(ATK) + " damage to " + enemy + ".")
+            if hp > 0:
+                HP -= atk
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+            input(">: ")
+            pass
+        elif choice == "2":
+            if pot > 0:
+                pot -= 1
+                heal(30)
+                HP -= atk
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+                input(">: ")
+            else:
+                print("You don't have any potions!")
+                input(">: ")
+
+            pass
+        elif choice == "3":
+            if mana > 0:
+                mana -= 1
+                heal(50)
+                HP -= atk
+                print(enemy + " dealt " + str(atk) + " damage to " + name + ".")
+                input(">: ")
+            else:
+                print("You have no mana!")
+                input(">: ")
+            pass
+
+        # play-result
+        if HP <= 0:
+            print(enemy + " defeated " + name + "....")
+            draw()
+            fight = False
+            play = False
+            run = False
+            print("GAME OVER")
+            input(">: Enter - Quit")
+        pass
+
+        if hp <= 0:
+            print(name + " defeated " + enemy + "....")
+            draw()
+            fight = False
+            gold += g
+            print("You've found " + str(g) + " gold!")
+            if random.randint(0, 100) < 30:
+                pot += 1
+                print("You've found a potion!")
+            if enemy == "Dragon":
+                draw()
+                print("CONGRATULATIONS, YOU HAVE DEFEATED THE DRAGON!!!, " + name)
+                boss = False
+                play = False
+                run = False
+            input(">: ")
+            clear()
 
 # Game-loop
 while run:
@@ -183,67 +421,135 @@ while run:
             quit()
 
     while play:
-        save() # @autosave
         # print(name)
+        save() # @autosave
         clear()
 
-        draw()
-        print("- LOCATION: "+ biom[map[y][x]]["t"])
-        print("- coordinates: ", x, y)
-        draw()
+        # enemy spawn::
+        if not standing:
+            if biom[map[y][x]]['e']:
+                if random.randint(0, 100) < 0: # less than 30 as value
+                    fight = True
+                    battle()
+        
+        if play:
 
-        if name == "":
-            name = "UNKNOWN-HERO"
-        print("- NAME: " + name)
-        print("- HP-HP: " + str(HP) + " /" + str(MAXHP))
-        print("- ATTCK: " + str(ATK))
-        print("- POTIN: " + str(pot))
-        print("- MANA.: " + str(mana))
-        print("- GOLD.: " + str(gold))
+            draw()
+            print("- LOCATION: "+ biom[map[y][x]]["t"])
+            print("- coordinates: ", x, y)
+            draw()
 
-        print(":- Enter '0' to save and quit -:")
-        draw()
+            if name == "":
+                name = "UNKNOWN-HERO"
+            print("- NAME: " + name)
+            print("- HP-HP: " + str(HP) + " /" + str(MAXHP))
+            print("- ATTAK: " + str(ATK))
+            print("- POTIN: " + str(pot))
+            print("- MANA.: " + str(mana))
+            print("- GOLD.: " + str(gold))
 
-        # player movement
-        # print("\n")
-        if y > 0:
-            print("1 - NORTH")
-        if x < x_len:
-            print("2 - EAST")
-        if y < y_len:
-            print("3 - SOUTH")
-        if x > 0:
-            print("4 - WEST")
-        draw()
+            print(":- Enter '0' to save and quit -:")
+            draw()
 
-        # destination
-        dest = input("# ")
-
-        if dest == '0':
-            play = False
-            menu = True
-            save() # go back to menu save
-            input(">: Enter to quit")
-            # quit()
-        elif dest == "1":
+            # player movement
+            # print("\n")
             if y > 0:
-                y -= 1
-            # else:
-            #     # y = y_len # open border
-            #     y = 0 # close border
-            pass
-        elif dest == "2":
+                print("1 - NORTH")
             if x < x_len:
-                x += 1
-            pass
-        elif dest == "3":
+                print("2 - EAST")
             if y < y_len:
-                y += 1
-            # else:
-            #     # y = 0 open border
-            #     y = y_len # close border
-            pass
-        elif dest == "4":
+                print("3 - SOUTH")
             if x > 0:
-                x -= 1
-            pass
+                print("4 - WEST")
+            draw()
+            if pot > 0:
+                print("5 - USE POTION (30HP)")
+            if mana > 0:
+                print("6 - USE MANA (50HP)")
+            if pot == 0 and mana == 0: print("NO ITEMS or MAGIC")
+
+            if map[y][x] == 'shop' or map[y][x] == 'mayor' or map[y][x] == 'cave':
+                print("7 - INTERACT")
+            draw()
+                
+
+            # destination
+            dest = input("# ")
+
+            if dest == '0':
+                play = False
+                menu = True
+                save() # go back to menu save
+                input(">: Enter to quit")
+                # quit()
+            elif dest == "1":
+                if y > 0:
+                    y -= 1
+                    standing = False
+                # else:
+                #     # y = y_len # open border
+                #     y = 0 # close border
+                pass
+            elif dest == "2":
+                if x < x_len:
+                    x += 1
+                    standing = False
+
+                pass
+            elif dest == "3":
+                if y < y_len:
+                    y += 1
+                    standing = False
+
+                # else:
+                #     # y = 0 open border
+                #     y = y_len # close border
+                pass
+            elif dest == "4":
+                if x > 0:
+                    x -= 1
+                    standing = False
+                pass
+            elif dest == "5":
+                if pot > 0:
+                    pot -= 1
+                    heal(30)
+                else:
+                    print("You have no potion!")
+                input(">: ")
+                standing = True
+            elif dest == "6":
+                if mana > 0:
+                    mana -= 1
+                    heal(50)
+                else:
+                    print("You have no mana!")
+                input(">: ")
+                standing = True
+            elif dest == "7":
+                if map[y][x] == 'shop':
+                    buy = True
+                    shop()
+                if map[y][x] == 'mayor':
+                    speak = True
+                    mayor()
+                if map[y][x] == 'cave':
+                    boss = True
+                    cave()
+            else: standing = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
